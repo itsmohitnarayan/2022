31e5a481d0bd9daa3469e2850da3556732c3ef64
Mossad <68772712+mohit724196@users.noreply.github.com>
2022-09-16 22:27:55 +0530

    Add files via upload

---
+++ Python/2.py
@@ -0,0 +1,18 @@
+print("Hello world")
+wallet=41
+print(wallet)
+wallet=32
+print(wallet)
+
+day=21
+temp= -15
+weight=60.9
+print(day+3)
+print(weight)
+print(temp)
+
+movie='dr strange "multiverse of madness\"'
+print(movie)
+
+
+
---
+++ Python/3.py
@@ -0,0 +1,12 @@
+day=16
+month="september"
+temp=-24
+day_name="friday"
+print(f"today is {month} {day} {day_name} and it's {temp} degree outside")
+
+fan_is_on=True
+
+if fan_is_on:
+    print("the fan is on!")
+else:
+    print("we're in dark")
