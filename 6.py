824ac7f773244cf63fd75ba92d9c1e2050c4848f
Kumar Mohit <68772712+itsmohitnarayan@users.noreply.github.com>
2022-09-17 22:44:53 +0530

    Delete 6.py

--- 6.py
+++
@@ -1,6 +0,0 @@
-user_text=input("enter some text: ")
-upper_or_lower = input("Enter 1 for upper case and 2 for lower case:")
-if upper_or_lower =="1":
-    print(user_text.upper())
-else:
-    print(user_text.lower())
